9a38c4adb1ebb29770b42db9247b11561290c3cb
Kamil Milewski <kmilewski@student.agh.edu.pl>
2023-11-24 12:15:23 +0100

    synchro

--- players/players.py
+++ players/players.py
@@ -23,13 +23,13 @@ class Player(pygame.sprite.Sprite):
 
     
 class Obstacle:
-    def __init__(self,width,height,x,y):
+    def __init__(self, width, height, x, y):
         self.obstacle_width = width
         self.obstacle_height = height
         self.obstacle_x = x
         self.obstacle_y = y
     def return_parameters(self):
-        return (self.obstacle_x, self.obstacle_y, self.obstacle_width, self.obstacle_height)
+        return self.obstacle_x, self.obstacle_y, self.obstacle_width, self.obstacle_height
     
 
 middle_line = Obstacle(middle_line_width,screen_height,screen_width//2 - middle_line_width//2,0)
