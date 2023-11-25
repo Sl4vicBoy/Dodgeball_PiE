77d44e3490289318bb31fa660fab884738d18b07
Kamil Milewski <kmilewski@student.agh.edu.pl>
2023-11-25 13:50:05 +0100

    Fixed spawn points

--- scripts/player.py
+++ scripts/player.py
@@ -1,9 +1,11 @@
 import pygame
 from constant_values import SCREEN_WIDTH, SCREEN_HEIGHT, BORDERS_PARAMETER, RED, BLUE
 
+
 class Player:
     RADIUS = 20
     VEL = 4
+
     def __init__(self, team, x, y):
         self.team = int(team)
         self.x = x
@@ -14,25 +16,27 @@ class Player:
         else:
             self.color = BLUE
 
-    def move(self):
+    def move(self, team):
         keys = pygame.key.get_pressed()
 
         match self.team:
             case 1:  # RIGHT
-                if keys[pygame.K_LEFT] and self.x - Player.VEL > (SCREEN_WIDTH + BORDERS_PARAMETER) / 2 + Player.RADIUS / 2:
+                if keys[pygame.K_LEFT] and self.x - Player.VEL > (
+                        SCREEN_WIDTH + BORDERS_PARAMETER) / 2 + Player.RADIUS:
                     self.x -= Player.VEL
-                if keys[pygame.K_RIGHT] and self.x + Player.VEL < SCREEN_WIDTH - BORDERS_PARAMETER - Player.RADIUS / 2:
+                if keys[pygame.K_RIGHT] and self.x + Player.VEL < SCREEN_WIDTH - BORDERS_PARAMETER - Player.RADIUS:
                     self.x += Player.VEL
 
             case 0:  # LEFT
-                if keys[pygame.K_LEFT] and self.x - Player.VEL > BORDERS_PARAMETER + Player.RADIUS / 2:
+                if keys[pygame.K_LEFT] and self.x - Player.VEL > BORDERS_PARAMETER + Player.RADIUS:
                     self.x -= Player.VEL
-                if keys[pygame.K_RIGHT] and self.x + Player.VEL < (SCREEN_WIDTH - BORDERS_PARAMETER) / 2 - Player.RADIUS / 2:
+                if keys[pygame.K_RIGHT] and self.x + Player.VEL < (
+                        SCREEN_WIDTH - BORDERS_PARAMETER) / 2 - Player.RADIUS:
                     self.x += Player.VEL
 
-        if keys[pygame.K_UP] and self.y - Player.VEL > Player.RADIUS / 2 + BORDERS_PARAMETER:
+        if keys[pygame.K_UP] and self.y - Player.VEL > Player.RADIUS + BORDERS_PARAMETER:
             self.y -= Player.VEL
-        if keys[pygame.K_DOWN] and self.y + Player.VEL < SCREEN_HEIGHT - Player.RADIUS / 2 - BORDERS_PARAMETER:
+        if keys[pygame.K_DOWN] and self.y + Player.VEL < SCREEN_HEIGHT - Player.RADIUS - BORDERS_PARAMETER:
             self.y += Player.VEL
 
     def draw(self, screen):
