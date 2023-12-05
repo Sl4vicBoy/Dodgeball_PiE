595ecbd0bafbc6d532a76bf35fb2f480005d0791
Dawid Hebda <dawhebda@gmail.com>
2023-12-05 12:14:07 +0100

    Try to include stages

--- scripts/obstacle.py
+++ scripts/obstacle.py
@@ -1,10 +1,9 @@
 import pygame.draw
-from random import randint
-from constant_values import BORDER_COLOR, SCREEN_HEIGHT, SCREEN_WIDTH
+from constant_values import BORDER_COLOR
 
 
-class Obstacle(pygame.sprite.Sprite): #niezniszczalna przeszkoda 
-    def __init__(self, width = 10, height = 10, x = 10, y = 10, color = BORDER_COLOR):
+class Obstacle(pygame.sprite.Sprite):  # niezniszczalna przeszkoda
+    def __init__(self, width, height, x, y, color=BORDER_COLOR):
         super().__init__()
         self.image = pygame.Surface((width, height))
         self.rect = self.image.get_rect()
@@ -17,8 +16,8 @@ class Obstacle(pygame.sprite.Sprite): #niezniszczalna przeszkoda
         pygame.draw.rect(screen, self.color, self.rect)
 
 
-class Midline(Obstacle): # linia po srodku 
-    def __init__(self, width, height, x, y, color = 'violet'):
+class Midline(Obstacle):  # linia po srodku
+    def __init__(self, width, height, x, y, color='violet'):
         super().__init__(width, height, x, y, color)
         self.collision_ball = False
 
@@ -28,7 +27,7 @@ class Midline(Obstacle): # linia po srodku
 
 class DestroyableObstacle(Obstacle):
     def __init__(self, width, height, x, y, color, time_to_live):
-        super().__init__()
+        super().__init__(width, height, x, y, color)
         self.time_to_live = time_to_live
     
     def destroy(self):
