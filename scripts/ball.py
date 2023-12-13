bfc231eab1dd69d1a497cb901ceed60c3d20ccaa
Dawid Hebda <dawhebda@gmail.com>
2023-12-13 11:06:27 +0100

    Integration of ball sprite

--- scripts/ball.py
+++ scripts/ball.py
@@ -1,15 +1,18 @@
 import pygame
 from constant_values import SCREEN_WIDTH, SCREEN_HEIGHT, FPS
 from random import uniform
+import os
 
 
 class Ball(pygame.sprite.Sprite):
     COLOR = 'Black'
-    RADIUS = 10
+    DIAMETER = 20
 
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((Ball.RADIUS * 2, Ball.RADIUS * 2))
+        ball_img = pygame.image.load(os.path.join('Assets', 'balls', 'basket-ball.png')).convert_alpha()
+        ball_img_scaled = pygame.transform.scale(ball_img, (self.DIAMETER, self.DIAMETER))
+        self.image = ball_img_scaled
         self.rect = self.image.get_rect(center=(x, y))
         self.vel = pygame.math.Vector2
         self.dvel = pygame.math.Vector2
@@ -17,9 +20,6 @@ class Ball(pygame.sprite.Sprite):
     def def_rand_vel(self):
         self.vel = pygame.math.Vector2(uniform(1, 5), uniform(1, 5))
         self.dvel = pygame.math.Vector2(self.vel.x / (FPS ** 2), self.vel.y / (FPS ** 2))
-
-    def draw(self, screen):
-        pygame.draw.circle(screen, Ball.COLOR, self.rect.center, Ball.RADIUS)
 
     def move(self):  # how we move the ball, always plus vel can be positive or negative
         self.rect.center += self.vel
@@ -31,7 +31,7 @@ class Ball(pygame.sprite.Sprite):
             self.vel.x *= -1
 
     def check_collision_player(self, players_playing):
-        collision = pygame.sprite.spritecollide(self, players_playing, False)
+        collision = pygame.sprite.spritecollide(self, players_playing, False, pygame.sprite.collide_mask)
         if collision:
             player = collision[0]
             # self.vel.xy = (0, 0)
@@ -39,13 +39,13 @@ class Ball(pygame.sprite.Sprite):
         return collision
 
     def check_collision_obstacle(self, obstacles):
-        collision = pygame.sprite.spritecollide(self, obstacles, False)
+        collision = pygame.sprite.spritecollide(self, obstacles, False, pygame.sprite.collide_mask)
         
         if collision:
             obstacle = collision[0]
             # Check left and right sides of the obstacle
-            if (self.rect.bottom <= obstacle.rect.bottom + 2 * self.RADIUS and
-                    self.rect.top >= obstacle.rect.top - 2 * self.RADIUS):
+            if (self.rect.bottom <= obstacle.rect.bottom + self.DIAMETER and
+                    self.rect.top >= obstacle.rect.top - self.DIAMETER):
 
                 if self.rect.right <= obstacle.rect.left:
                     self.rect.center -= (2 * self.dvel.x, 0)
