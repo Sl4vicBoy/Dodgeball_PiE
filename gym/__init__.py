4bdabbb2099d2b464906fb2e5d50d9d38d8933b2
Aleksander Kwaśny <aleksanderkwasnyy@gmail.com>
2024-01-19 19:08:27 +0100

    registration

--- gym/__init__.py
+++ gym/__init__.py
@@ -2,5 +2,5 @@ from gym.envs.registration import register
 
 register(
     id="Dodgeball-v0",
-    entry_point="gym.env.dodgeball.py:DodgeballEnv",
+    entry_point="env.dodgeball.py:DodgeballEnv",
 )
